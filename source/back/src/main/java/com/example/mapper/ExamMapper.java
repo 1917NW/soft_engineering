d477f676199062578f0bdb9bbef2f573d5bc4773
MAG1CPI <2947928186@qq.com>
2023-06-14 21:55:24 +0800

    exam

---
+++ source/back/src/main/java/com/example/mapper/ExamMapper.java
@@ -0,0 +1,16 @@
+package com.example.mapper;
+
+import com.example.entity.Exam;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author zw
+ * @since 2023-05-18
+ */
+public interface ExamMapper extends BaseMapper<Exam> {
+
+}
---
+++ source/back/src/main/java/com/example/service/IExamService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import com.example.entity.Exam;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author zw
+ * @since 2023-05-18
+ */
+public interface IExamService extends IService<Exam> {
+
+}
