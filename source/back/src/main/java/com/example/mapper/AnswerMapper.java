91c0c90c3f28471b21e191d8d24da55f669c9c22
Kaixuan <lxk686868@outlook.com>
2023-06-17 11:16:47 +0800

    control&map

---
+++ source/back/src/main/java/com/example/mapper/AnswerMapper.java
@@ -0,0 +1,16 @@
+package com.example.mapper;
+
+import com.example.entity.Answer;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author lxk
+ * @since 2023-05-21
+ */
+public interface AnswerMapper extends BaseMapper<Answer> {
+
+}
