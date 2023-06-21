32cfd3414d53dbafce7bc6c5c55617c84843fd8a
Kaixuan <lxk686868@outlook.com>
2023-06-21 16:21:17 +0800

    service

---
+++ source/back/src/main/java/com/example/service/IAnswerService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import com.example.entity.Answer;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lxk
+ * @since 2023-05-22
+ */
+public interface IAnswerService extends IService<Answer> {
+
+}
